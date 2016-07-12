764dc2f25a7f3338b1f805b522d7e319fe439c48
Maynard Russell <maynard.russell@gmail.com>
2016-07-11 22:43:45 -0400

    Radio
    Setup radio button replacement

--- gallery.js
+++ gallery.js
@@ -1,7 +1,27 @@
+// Dropdown
+$(document).on("click", ".dropOptions li", function (ev) {
+    //TODO: Can't catch click with focus logic :-/
+    var el = $(this);
+    el.closest(".dropOptions").prev(".dropLabel").text(el.text());
+});
+
+// Radio Buttons
+$(document).on("change", ".radioGroup", function (ev) {
+    var el = $(this);
+    var target = el.find('input:checked').closest(".radio");
+    var position = target.offset().left - el.offset().left + 6;
+
+    var dot = el.find(".radioDot");
+    dot.css("transform", "scale(0.6)");
+    dot.css({ left: position });
+    setTimeout(function () { dot.css("transform", ""); }, 200);
+
+});
+
 // Card effects
 $(document).on("click", ".cardBox", function (ev) {
     var el = $(this);
-    var overlay = $(".cardOverlay")
+    var overlay = $(".cardOverlay");
     var target = overlay.find(".overlayContent");
     target.append(el.clone());                  // Copy contents into new box, which will undergo the transition
     target.css(el.position());                  // Move box to starting position above original box
